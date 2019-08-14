d16423dc050a04b64fe0396f1553ef260c1fbf17
Rabbit <dawoodahmed696@gmail.com>
2019-08-14 23:16:47 +0500

    imported products module

--- src/store/index.js
+++ src/store/index.js
@@ -1,6 +1,7 @@
 import Vuex from 'vuex';
 import Vue from 'vue';
 import suppliers from './modules/suppliers';
+import products from './modules/products';
 
 // Load Vuex
 Vue.use(Vuex);
@@ -8,6 +9,7 @@ Vue.use(Vuex);
 // Create store
 export default new Vuex.Store({
     modules: {
-        suppliers
+        suppliers,
+        products
     }
 });
