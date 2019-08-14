b9aaa54fcaa9e1fbcd9b055d7c8740ad1bcbea2a
Rabbit <dawoodahmed696@gmail.com>
2019-08-14 22:24:49 +0500

    suppliers ui view update delete

--- src/pages/Suppliers.vue
+++ src/pages/Suppliers.vue
@@ -7,72 +7,120 @@
         <md-card>
           <md-card-header data-background-color="green">
              <div class="md-button-content ">
-              <i data-v-393bd7a0=""  v-b-modal.modal-scoped class="md-icon md-icon-font md-theme-demo-light pull-right"> add</i>
+              <i data-v-393bd7a0=""  @click="createSupplier()" class="md-icon md-icon-font md-theme-demo-light pull-right"> add</i>
               </div> 
             <h4 class="title">Suppliers</h4>
             <p class="category">Manage your Business Suppliers</p>
           </md-card-header>
           <md-card-content>
-            <data-table table-header-color="green" title="Sparco Users" :fields="fields" :tableData="suppliers"></data-table>
+            <data-table table-header-color="green" @item-event="showModal" title="Sparco Users" :fields="fields" :tableData="suppliers"></data-table>
           </md-card-content>
         </md-card>
       </div>
     </div>
-         <b-modal id="modal-scoped">
-    <template slot="modal-header" slot-scope="{ close }">
-      <!-- Emulate built in modal header close button action -->
-      <!-- <b-button size="sm" variant="outline-danger" @click="close()">
-        C
-      </b-button> -->
-      <h5>Create Supplier</h5>
-    </template>
+    <b-modal id="create-supplier-modal">
+      <template slot="modal-header" slot-scope="{ close }">
+        <h5>Create Supplier</h5>
+      </template>
 
-    <template slot="default" slot-scope="{ hide }">
-      <div class="row">
-              <div  class="col-md-6">
-                <h5 style="color:black;">Full Name</h5>
-              </div>
-              <div class="col-md-6">
-                <b-form-input v-model="supplier_obj.full_name" placeholder="Alex Hesh"></b-form-input>
-              </div>
-              <div  class="col-md-6">
-                <h5 style="color:black;">Location</h5>
-              </div>
-              <div class="col-md-6">
-                <b-form-input v-model="supplier_obj.location" placeholder="Saint Preux"></b-form-input>
-              </div>
-              <div  class="col-md-6">
-                <h5 style="color:black;">Debit</h5>
-              </div>
-              <div class="col-md-6">
-                <b-form-input v-model="supplier_obj.debit"  placeholder="$300"></b-form-input>
-              </div>
-              <div  class="col-md-6">
-                <h5 style="color:black;">Credit</h5>
-              </div>
-              <div class="col-md-6">
-                <b-form-input v-model="supplier_obj.credit" placeholder="$300"></b-form-input>
-              </div>
-              <div  class="col-md-6">
-                <h5 style="color:black;">Balance</h5>
-              </div>
-              <div class="col-md-6">
-                <b-form-input v-model="supplier_obj.balance" placeholder="$300"></b-form-input>
-              </div>
-              
-        </div>
-    </template>
+      <template slot="default" slot-scope="{ hide }">
+        <div class="row">
+                <div  class="col-md-6">
+                  <h5 style="color:black;">Full Name</h5>
+                </div>
+                <div class="col-md-6">
+                  <b-form-input v-model="supplier_obj.full_name" placeholder="Alex Hesh"></b-form-input>
+                </div>
+                <div  class="col-md-6">
+                  <h5 style="color:black;">Location</h5>
+                </div>
+                <div class="col-md-6">
+                  <b-form-input v-model="supplier_obj.location" placeholder="Saint Preux"></b-form-input>
+                </div>
+                <div  class="col-md-6">
+                  <h5 style="color:black;">Debit</h5>
+                </div>
+                <div class="col-md-6">
+                  <b-form-input v-model="supplier_obj.debit"  placeholder="$300"></b-form-input>
+                </div>
+                <div  class="col-md-6">
+                  <h5 style="color:black;">Credit</h5>
+                </div>
+                <div class="col-md-6">
+                  <b-form-input v-model="supplier_obj.credit" placeholder="$300"></b-form-input>
+                </div>
+                <div  class="col-md-6">
+                  <h5 style="color:black;">Balance</h5>
+                </div>
+                <div class="col-md-6">
+                  <b-form-input v-model="supplier_obj.balance" placeholder="$300"></b-form-input>
+                </div>
+                
+          </div>
+      </template>
 
-    <template slot="modal-footer" slot-scope="{ ok, cancel, hide }">
-      <b-button size="sm" variant="danger" @click="cancel()">
-        Cancel
-      </b-button>
-      <b-button style="margin-left:5px; margin-bottom:0px !important" size="sm" variant="success" @click="callFillSuppliers()">
-        Save
-      </b-button>
- 
-    </template>
-  </b-modal>
+      <template slot="modal-footer" slot-scope="{ ok, cancel, hide }">
+        <b-button size="sm" variant="danger" @click="cancel()">
+          Cancel
+        </b-button>
+        <b-button style="margin-left:5px; margin-bottom:0px !important" size="sm" variant="success" @click="callFillSuppliers()">
+          Save
+        </b-button>
+  
+      </template>
+    </b-modal>
+
+    <b-modal id="update-supplier-modal">
+      <template slot="modal-header" slot-scope="{ close }">
+        <h5>Modify Supplier</h5>
+      </template>
+
+      <template slot="default" slot-scope="{ hide }">
+        <div class="row">
+                <div  class="col-md-6">
+                  <h5 style="color:black;">Full Name</h5>
+                </div>
+                <div class="col-md-6">
+                  <b-form-input v-model="supplier_obj.full_name" placeholder="Alex Hesh"></b-form-input>
+                </div>
+                <div  class="col-md-6">
+                  <h5 style="color:black;">Location</h5>
+                </div>
+                <div class="col-md-6">
+                  <b-form-input v-model="supplier_obj.location" placeholder="Saint Preux"></b-form-input>
+                </div>
+                <div  class="col-md-6">
+                  <h5 style="color:black;">Debit</h5>
+                </div>
+                <div class="col-md-6">
+                  <b-form-input v-model="supplier_obj.debit"  placeholder="$300"></b-form-input>
+                </div>
+                <div  class="col-md-6">
+                  <h5 style="color:black;">Credit</h5>
+                </div>
+                <div class="col-md-6">
+                  <b-form-input v-model="supplier_obj.credit" placeholder="$300"></b-form-input>
+                </div>
+                <div  class="col-md-6">
+                  <h5 style="color:black;">Balance</h5>
+                </div>
+                <div class="col-md-6">
+                  <b-form-input v-model="supplier_obj.balance" placeholder="$300"></b-form-input>
+                </div>
+                
+          </div>
+      </template>
+
+      <template slot="modal-footer" slot-scope="{ ok, cancel, hide }">
+        <b-button size="sm" variant="danger" @click="cancel()">
+          Delete
+        </b-button>
+        <b-button style="margin-left:5px; margin-bottom:0px !important" size="sm" variant="success" >
+          Update
+        </b-button>
+  
+      </template>
+    </b-modal>
 
           
   </div>
@@ -113,10 +161,13 @@ export default {
   methods:{
 
     callFillSuppliers(){
-      console.log(this.supplier_obj)
-      this.$bvModal.hide('modal-scoped')
+      this.$bvModal.hide('create-supplier-modal')
       //api code here
       this.$store.commit("newSupplier",this.supplier_obj)
+      this.emptySupplierObj()
+      
+    },
+    emptySupplierObj(){
       this.supplier_obj={
         full_name:null,
         credit:null,
@@ -125,6 +176,23 @@ export default {
         balance:null,
 
       }
+
+    },
+    showModal(item){
+      this.supplier_obj={
+        full_name:item.full_name,
+        credit:item.credit,
+        debit:item.debit,
+        location:item.location,
+        balance:item.balance,
+
+      }
+      this.$bvModal.show('update-supplier-modal')
+
+    },
+    createSupplier(){
+      this.emptySupplierObj()
+      this.$bvModal.show('create-supplier-modal')
 
     }
 
