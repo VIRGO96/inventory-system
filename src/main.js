7d80bbc02017908d63c2f0d7ad9d68eb63e0bdf0
Rabbit <dawoodahmed696@gmail.com>
2019-08-14 13:08:46 +0500

    bootstrap css imported

--- src/main.js
+++ src/main.js
@@ -17,7 +17,10 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
 import App from "./App";
-
+import BootstrapVue from 'bootstrap-vue'
+Vue.use(BootstrapVue)
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
 // router setup
 import routes from "./routes/routes";
 
