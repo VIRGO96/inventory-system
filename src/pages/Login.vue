65d404ff853507fd904b95cf7b4d659c1af9c3e6
Rabbit <dawoodahmed696@gmail.com>
2019-08-16 10:15:22 +0500

    login logo image

--- src/pages/Login.vue
+++ src/pages/Login.vue
@@ -6,6 +6,12 @@
 					Account Login
 				</span>
 				<form class="login100-form validate-form p-b-33 p-t-5">
+					<div class="row">
+						<div class="offset-md-3 col-md-6">
+          <img src="../assets/img/vue-logo.jpeg" alt="" style="width: 160px;height: 100px;"/>
+
+						</div>
+					</div>
 
 					<div class="wrap-input100 validate-input" >
                         
