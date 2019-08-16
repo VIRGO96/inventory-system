ec71c13ef9bf43712e5f1c637214b7c28ede382a
Rabbit <dawoodahmed696@gmail.com>
2019-08-16 13:26:47 +0500

    updated little headings

--- src/pages/Login.vue
+++ src/pages/Login.vue
@@ -3,7 +3,7 @@
 		<div class="container-login100" style="background-image: url('images/bg-01.jpg');">
 			<div class="wrap-login100 p-t-30 p-b-50">
 				<span class="login100-form-title p-b-41">
-					Account Login
+				<b><strong><i>Account Login</i></strong></b>	
 				</span>
 				<form class="login100-form validate-form p-b-33 p-t-5">
 					<div class="row">
@@ -15,7 +15,7 @@
 
 					<div class="wrap-input100 validate-input" >
                         
-						<input class="input100" type="text" name="username" placeholder="User name">
+						<input class="input100" type="text" name="username" placeholder="Username">
                         <span class="focus-input100" style="top:44px;left:35px;">
                             <i class="fas fa-user" style="font-size:24px"></i>
                         </span> 
